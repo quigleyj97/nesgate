68671c7b87fd8cab0ac1624518158d08c1f18f9c
Joe Quigley <quigley.joseph@outlook.com>
2025-03-19 18:55:12 -0700

    Fix the synthesis of the phased clock generator
    It seems the previous implementation wasn't synthesizable, despite simulating OK and synthesizing with ghdl --synth. This works with yosys and synthesizes correctly.

--- cores/phased_clock_generator/core.vhdl
+++ cores/phased_clock_generator/core.vhdl
@@ -4,13 +4,23 @@ use ieee.std_logic_1164.all;
 entity phased_clock_generator is
     port(
         clk_in: in std_ulogic;
-        clk_phi1: inout std_ulogic;
-        clk_phi2: inout std_ulogic
+        clk_out: out std_ulogic;
+        clk_phi1: out std_ulogic;
+        clk_phi2: out std_ulogic
     );
 end entity;
 
 architecture rtl of phased_clock_generator is
+    signal clk_internal: std_ulogic := '0';
 begin
-    clk_phi1 <= clk_in nor clk_phi2;
-    clk_phi2 <= clk_in and not clk_phi1;
+    process(clk_in)
+    begin
+        if rising_edge(clk_in) then
+            clk_internal <= not clk_internal;
+        end if;
+    end process;
+
+    clk_out <= clk_internal;
+    clk_phi1 <= not clk_internal;
+    clk_phi2 <= clk_internal;
 end rtl;
